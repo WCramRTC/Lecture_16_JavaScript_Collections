640f949985d0012ca7e796d342f0df4b27b4e2ad
WCram <wcram@rtc.edu>
2022-03-08 10:08:27 -0800

    Update

--- script/script.js
+++ script/script.js
@@ -1,1 +1,37 @@
-alert();
+const display = document.getElementById("display");
+const userInput = document.getElementById("user-input");
+const btnAdd = document.getElementById("btn-add");
+const btnNew = document.getElementById('btn-new');
+const btnClear = document.getElementById('btn-clear');
+
+
+
+
+btnAdd.addEventListener('click', e => {
+    let input = userInput.value;
+    DisplayAppend(input)
+})
+
+btnNew.addEventListener('click', e => {
+    let input = userInput.value;
+    DisplayNewLine(input)
+})
+
+btnClear.addEventListener('click', e => {
+    DisplayClear();
+})
+
+
+// Helper Methods
+
+function DisplayClear() {
+    display.innerText = "";
+}
+
+function DisplayAppend(content) {
+    display.innerText += content;
+}
+
+function DisplayNewLine(content) {
+    display.innerText += content + "\n";
+}
